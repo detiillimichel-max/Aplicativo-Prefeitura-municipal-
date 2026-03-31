bf5cf6d36de1407515a69748a550adc6ab95a4e8
Michel Detiilli <detiillimichel@gmail.com>
2026-03-31 05:45:11 -0300

    Create supabase.js

---
+++ assets/supabase.js
@@ -0,0 +1,6 @@
+const URL = 'https://svqocghixhrpqaxucubn.supabase.co';
+const KEY = 'sb_publishable_lE2ZPtlhTh6OWr3S9Fb3rg_rLXHjkHH';
+
+// Inicializa e exporta a instância
+export const supabase = window.supabase.createClient(URL, KEY);
+
